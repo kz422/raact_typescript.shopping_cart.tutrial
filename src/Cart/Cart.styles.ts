8d87c3a1ac713f146159a2aab6dd0b9bda4b03b4
kz422 <p.inoue.p@gmail.com>
2021-08-16 01:03:25 +0900

    initial commit

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,8 @@
+import { style } from "@material-ui/system";
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+  font-family: 'Courier New', Courier, monospace;
+  width: 500px;
+  padding: 20px;
+`
